635864bfdb6450d75332e05aaa3cafbfa34c86e7
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-08-02 13:16:02 +0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pytz  # Import the pytz library
 # Replace 'YOUR_TELEGRAM_BOT_TOKEN' with the token you obtained from BotFather
 bot = telebot.TeleBot(os.getenv('tg_key'))
 
-# Set the timezone to Limassol, Cyprus (GMT+3)
+# Set the timezone to any you want
 tz = pytz.timezone('Asia/Nicosia')
 
 keep_alive()
