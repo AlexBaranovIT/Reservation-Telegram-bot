624566a17fc9fb1e2509015af459f59f3a625e4b
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-10-22 11:13:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from PIL import Image, ImageDraw, ImageFont
 # Replace 'YOUR_TELEGRAM_BOT_TOKEN' with the token you obtained from BotFather
 bot = TeleBot(os.getenv('tg_key'))
 
-# Set the timezone to Limassol, Cyprus (GMT+3)
+# Set the timezone to Nicosia, Cyprus (GMT+3)
 tz = pytz.timezone('Asia/Nicosia')
  
 #Call keep_alive function to connect to the flask server
